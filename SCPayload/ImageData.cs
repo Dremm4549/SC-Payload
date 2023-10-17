32489972ee6d22b59aad55fdc84cba3167d8835d
NickHarrison <nharrison@conestogac.on.ca>
2023-10-17 13:44:20 -0400

    SPL-332- set up the two main function defintions for the ImageData class

---
+++ SCPayload/ImageData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCPayload
+{
+    internal class ImageData
+    {
+       
+
+        public void downloadImage()
+        {
+
+        }
+
+        public void getImage()
+        {
+            //rerturns image buffer
+        }
+    }
+}
