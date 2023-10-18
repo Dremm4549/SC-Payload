0b2ba393a0aa559f498bf2797e8a52e15d3101e4
NickHarrison <nharrison@conestogac.on.ca>
2023-10-18 19:57:23 -0400

    created the telem obj and its getters and setters

---
+++ SCPayload/Telem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCPayload
+{
+    internal class Telem
+    {
+        private float Long{ get; set; }
+        private float Lat { get; set; }
+        private float Temp { get; set; }
+
+
+    }
+}
