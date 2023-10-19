27de7a599238960f785849164d610ffd7cb2ff8c
NickHarrison <nharrison@conestogac.on.ca>
2023-10-19 17:10:29 -0400

    added all comments for doxygen documentation

--- SCPayload/Telem.cs
+++ SCPayload/Telem.cs
@@ -6,27 +6,51 @@ using System.Threading.Tasks;
 
 namespace SCPayload
 {
-     
+     /// <summary>
+     /// this class handles and changes or requests for data of the telemarty
+     /// </summary>
     public class Telem
-    {        
+    {
+        /// <summary>
+        /// variable for the tempature data type is a float and it has a private set and public get
+        /// </summary>
         public float Long{ get; private set; }
+        /// <summary>
+        /// variable for the tempature data type is a float and it has a private set and public get
+        /// </summary>
         public float Lat { get; private set; }
+        /// <summary>
+        /// variable for the tempature data type is a float and it has a private set and public get
+        /// </summary>
         public float Temp { get; private set; }
 
+        /// <summary>
+        /// default constructor for the telematry object will set all values to 0 by default
+        /// </summary>
         public Telem()
         {
             Long = 0;
             Lat = 0; 
             Temp = 0;
         }
-
+        /// <summary>
+        /// this is a paramaterized constructor to set a telematry object to a specific state 
+        /// </summary>
+        /// <param name="x"> longatude as a float</param>
+        /// <param name="y"> latatude as a float</param>
+        /// <param name="c"> tempature in celcius as a float</param>
         public Telem(float x, float y, float c)
         {
             Long = x;
             Lat = y;
             Temp = c;
         }
-
+        /// <summary>
+        /// this function sets the telematry of the current telemtary object upon request to the new values
+        /// </summary>
+        /// <param name="x">longatude as a float</param>
+        /// <param name="y">latatude as a float</param>
+        /// <param name="c">tempature in celcius as a float</param>
         public void SetTelemetry(float x, float y, float c)
         {
             this.Long = x; 
