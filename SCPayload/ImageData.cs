582f757a9e7462b1b1a5cdd3b5407b765e3bfabb
NickHarrison <nharrison@conestogac.on.ca>
2023-10-19 16:39:19 -0400

    added doxygen to the image data class

--- SCPayload/ImageData.cs
+++ SCPayload/ImageData.cs
@@ -5,30 +5,40 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SCPayload
-{   
-    internal class ImageData
+{
+    /// <summary>
+    /// this class is responsable for handling all functions related to the image data
+    /// </summary>
+    public class ImageData
     {
-        byte[] data;//stores the image
+        /// <summary>
+        /// a byte[] member variable that holdds the current images data the data can be up to 2gb 
+        /// </summary>
+        byte[] data;
+        /// <summary>
+        /// the telemetry object to hold all telemetry data
+        /// </summary>
         Telem tel;
         ImageData()
         {
             data = null!;
         }
-
+        /// <summary>
+        /// reading the image from a text file and saving it to the image buffer
+        /// </summary>
         public void downloadImage()
         {
-            //reading the image from a text file and saving it to the image buffer
+            
         }
-
+        /// <summary>
+        /// return image buffer the buffer will be a byte[] that holds the binary data max size 2GB storage
+        /// </summary>
         public byte[] getImage()
         {
-          //return image buffer
+          
           return data;
         }
 
-        public float getlat() 
-        { 
-            return tel.Lat;
-        }
+      
     }
 }
--- SCPayload/Telem.cs
+++ SCPayload/Telem.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace SCPayload
 {
-    internal class Telem
-    {
+     
+    public class Telem
+    {        
         public float Long{ get; private set; }
         public float Lat { get; private set; }
         public float Temp { get; private set; }
