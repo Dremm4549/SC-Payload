4b82b5ed1a9acee2358b5ec74421727cb131d1cc
NickHarrison <nharrison@conestogac.on.ca>
2023-10-18 20:18:45 -0400

    added a telem object to the image data class

--- SCPayload/ImageData.cs
+++ SCPayload/ImageData.cs
@@ -8,15 +8,20 @@ namespace SCPayload
 {   
     internal class ImageData
     {
-        
+        byte[] data;//stores the image
+        ImageData()
+        {
+            data = null!;
+        }
         public void downloadImage()
         {
             //reading the image from a text file and saving it to the image buffer
         }
 
-        public void getImage()
+        public byte[] getImage()
         {
           //return image buffer
+          return data;
         }
     }
 }
