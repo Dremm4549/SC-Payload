d8ed68eccba050f94381b5da13df6abddb6a280f
Bryan Antoniou <90355171+BAntoniou@users.noreply.github.com>
2023-10-29 20:07:17 -0400

    Developed main to include 2 crow routes for now, just to set a baseline for future routes. These routes have not yet been tested.

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,23 @@
-#include<iostream>
+#define CROW_MAIN
+
+#include "crow_all.h"
+#include <iostream>
 
 int main()
 {
+	crow::SimpleApp app;
+
+	//All routes in this main are subject to modification. This just provides a baseline to use so that 1. We have an idea of what routes we need to handle and 2. Other services will be able to communicate with our service.
+	//Current routes are not necessarily final either. The syntax for the routes may need to be changed, some routes can be added/removed, logic obviously still needs to be added for the routes as well.
+	//It should also be known that as of now, these routes have not been tested to even see if they work properly. 
+	CROW_ROUTE(app, "/ChangeState")
+		([](const request& req, response& res) {
+			
+		});
+
+	CROW_ROUTE(app, "/Telemetry")
+		([](const request& req, response& res) {
+
+		});
 	return 0;
 }
