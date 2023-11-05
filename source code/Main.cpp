70d495a84675af81eca5277d8c9877ddf90f8307
DESKTOP-BN8AR6M\Michael <mDremo5093@conestogac.on.ca>
2023-11-05 18:58:15 -0500

    added payload header with functions to toggle power state. Addtionally added a route to request.

--- source code/Main.cpp
+++ source code/Main.cpp
@@ -1,23 +1,69 @@
 #define CROW_MAIN
 
 #include "crow_all.h"
+#include "Payload.h"
 #include <iostream>
 
 int main()
 {
 	crow::SimpleApp app;
+	Payload payloadObj; //create payload object
 
 	//All routes in this main are subject to modification. This just provides a baseline to use so that 1. We have an idea of what routes we need to handle and 2. Other services will be able to communicate with our service.
 	//Current routes are not necessarily final either. The syntax for the routes may need to be changed, some routes can be added/removed, logic obviously still needs to be added for the routes as well.
 	//It should also be known that as of now, these routes have not been tested to even see if they work properly. 
-	CROW_ROUTE(app, "/ChangeState")
-		([](const request& req, response& res) {
-			
-		});
+
+    // Correct the lambda function declaration
+
+	CROW_ROUTE(app, "/")([](){
+    return "Hello world";
+	});
+
+	CROW_ROUTE(app, "/Image")([](){
+    return "Hello world";
+	});
+
+    CROW_ROUTE(app, "/payloadState")
+	.methods("PUT"_method)
+    ([&payloadObj](const crow::request& req, crow::response& res) {
+		const char* state = req.url_params.get("state");
+		crow::json::wvalue jsonResp;
+
+		if(state){
+			if(strcmp(state, "true") == 0){
+				payloadObj.SetPowerState(true);
+				res.code = 200;
+				jsonResp["status"] = "success";
+				jsonResp["message"] = "Payload is on";
+			}
+			else if(strcmp(state, "false") == 0){
+				payloadObj.SetPowerState(false);
+				res.code = 200;
+				jsonResp["status"] = "success";
+				jsonResp["message"] = "Payload is off";
+			}
+		}
+
+		res.set_header("Content-Type", "application/json");
+        res.write(jsonResp.dump());
+        res.end();
+    });
 
 	CROW_ROUTE(app, "/Telemetry")
-		([](const request& req, response& res) {
+		([](const crow::request& req, crow::response& res) {
 
 		});
-	return 0;
+
+	CROW_ROUTE(app, "/DownloadImage")
+	([](const crow::request& req, crow::response& res) {
+
+	});
+
+	CROW_ROUTE(app, "/CaptureImage")
+	([](const crow::request& req, crow::response& res) {
+
+	});
+
+	app.port(27000).multithreaded().run();
+    return 0;
 }
---
+++ source code/Payload.h
@@ -0,0 +1,42 @@
+#pragma once
+
+/// <summary>
+/// this class will handle the power state and be responsible for holding the image data
+/// </summary>
+class Payload
+{
+private:
+	bool powerState;
+
+
+public:
+	/// <summary>
+	/// Default constructor for the payload object
+	/// </summary>
+	Payload()
+	{
+        powerState = false;
+	}
+	/// <summary>
+	/// Constructor to set power state
+	/// </summary>
+	/// <param name="state">Powerstate as a bool</param>
+	Payload(bool state) : powerState(state) {};
+	/// <summary>
+	/// Get the current powerState
+	/// </summary>
+	/// <returns>power state as a bool</returns>
+	bool GetPowerState()
+	{
+		return powerState;
+	}
+
+	/// <summary>
+	/// Set power state
+	/// </summary>
+	/// <param name="state">Powerstate as a bool</param>
+
+	void SetPowerState(bool state)	{
+        powerState = state;
+	}
+};
