71cfc8649c7a6cc5e0006461f1a50546bf43f47b
Ethan Coates <Ecoates17@hotmail.com>
2023-11-10 10:43:13 -0500

    Added better doxygen descriptions and made external libraries refrence SolutionDir

--- source code/ImageData.h
+++ source code/ImageData.h
@@ -4,30 +4,42 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-/// <summary>
-/// This class is expected to handle all image data functions
-/// </summary>
+/**
+ * @brief Class to handle image data operations.
+ *
+ * This class is responsible for managing image data, including reading from files
+ * and providing access to the image data. It uses a vector to store the image data
+ * in bytes and manages file operations for reading images.
+ */
+
 class ImageData
 {
 private:
-	std::vector<unsigned char> data;
-	Telem t;
-	int fileSize;
-	int totalBytesRead;
+	std::vector<unsigned char> data; ///< Buffer to store image data in bytes.
+	Telem t; ///< Telemetry object (if used for additional metadata or operations).
+	int fileSize; ///< Size of the image file in bytes.
+	int totalBytesRead; ///< Total number of bytes read from the file.
 
 public:
-	/// <summary>
-	/// Default constructor for ImageData
-	/// </summary>
+	/**
+	 * @brief Construct a new ImageData object.
+	 *
+	 * Initializes fileSize and totalBytesRead to zero and clears any existing data.
+	 */
 	ImageData()
 	{
 		fileSize = 0;
 		totalBytesRead = 0;
 		data.clear();
 	}
-	/// <summary>
-	/// Reading the image from a text file and saving it to the image buffer
-	/// </summary>
+	/**
+	 * @brief Reads an image from a file and stores it in the buffer.
+	 *
+	 * The function opens a file from a predefined path, reads its contents,
+	 * and stores it in an internal buffer. It handles file operations and errors.
+	 *
+	 * @exception std::runtime_error Thrown if the file cannot be opened.
+	 */
 	void downloadImage()
 	{
 		std::string filePath = "../../SpaceImages";
@@ -53,10 +65,14 @@ public:
 		delete[] imageBuffer;
 
 	}
-	/// <summary>
-	/// Return image buffer the buffer will be a byte[] that holds the binary data max size 2GB storage
-	/// </summary>
-	/// <returns>The image data as a vector</returns>
+	/**
+	 * @brief Get the Image data as a vector of bytes.
+	 *
+	 * This function provides access to the image data that has been read and stored.
+	 * The data is returned as a vector of unsigned char (bytes).
+	 *
+	 * @return std::vector<unsigned char> The image data.
+	 */
 	std::vector<unsigned char> getImage()
 	{
 		return data;
