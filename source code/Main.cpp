a1b4e2396be50c64bc9cfa89476b799568775fa6
snickk95 <51108751+snickk95@users.noreply.github.com>
2023-11-23 21:01:54 -0500

    added a check on telemetry route

--- source code/Main.cpp
+++ source code/Main.cpp
@@ -19,28 +19,28 @@ int main()
 	//Current routes are not necessarily final either. The syntax for the routes may need to be changed, some routes can be added/removed, logic obviously still needs to be added for the routes as well.
 	//It should also be known that as of now, these routes have not been tested to even see if they work properly. 
 
-    // Correct the lambda function declaration
+	// Correct the lambda function declaration
 
-	CROW_ROUTE(app, "/")([](){
-    return "Hello world";
-	});
+	CROW_ROUTE(app, "/")([]() {
+		return "Hello world";
+		});
 
 	/// <summary>
 	/// This route listens for a put request that and querys 
 	//  a true or false paramter from the url use to alter the state
 	/// </summary>
 
-    CROW_ROUTE(app, "/payloadState")
-	.methods("PUT"_method)
-    ([&payloadObj](const crow::request& req, crow::response& res) {
+	CROW_ROUTE(app, "/payloadState")
+		.methods("PUT"_method)
+		([&payloadObj](const crow::request& req, crow::response& res) {
 		const char* state = req.url_params.get("state");
 		crow::json::wvalue jsonResp;
-		if(state){
-			if(strcmp(state, "true") == 0 && payloadObj.GetPowerState() == false) {
+		if (state) {
+			if (strcmp(state, "true") == 0 && payloadObj.GetPowerState() == false) {
 				payloadObj.SetPowerState(true);
 				res.code = 200;
 			}
-			else if(strcmp(state, "false") == 0 && payloadObj.GetPowerState == true){
+			else if (strcmp(state, "false") == 0 && payloadObj.GetPowerState == true) {
 				payloadObj.SetPowerState(false);
 				res.code = 200;
 			}
@@ -51,17 +51,17 @@ int main()
 		}
 
 		res.set_header("Content-Type", "application/json");
-        res.write(jsonResp.dump());
-        res.end();
-    });
+		res.write(jsonResp.dump());
+		res.end();
+			});
 
 	/// <summary>
 	/// This route will return the current power state 
 	/// of the payload 
 	/// </summary>
 	CROW_ROUTE(app, "/GetPayloadState")
-	.methods("GET"_method)
-	([&payloadObj](const crow::request& req, crow::response& res) {
+		.methods("GET"_method)
+		([&payloadObj](const crow::request& req, crow::response& res) {
 		crow::json::wvalue jsonResp;
 		if (payloadObj.GetPowerState() == false)
 		{
@@ -72,11 +72,11 @@ int main()
 			jsonResp["data"]["message"] = payloadObj.GetPowerState();
 			res.code = 200;
 		}
-		
+
 		res.set_header("Content-Type", "application/json");
-        res.write(jsonResp.dump());
-        res.end();
-	});
+		res.write(jsonResp.dump());
+		res.end();
+			});
 
 	/// <summary>
 	/// This route will be responsible for setting the telemetry 
@@ -89,15 +89,21 @@ int main()
 	/// Gets the latest telemtery values from the telemetry object
 	/// </summary>
 	CROW_ROUTE(app, "/Telemetry")
-	.methods("GET"_method)
-		([&telemetryObj](const crow::request& req, crow::response& res) {
+		.methods("GET"_method)
+		([&telemetryObj, &payloadObj](const crow::request& req, crow::response& res) {
 			crow::json::wvalue jsonResp;
+			if (payloadObj.GetPowerState() == false)
+			{
+				res.code = 503;
+			}
+			else
+			{
+				res.code = 200;
+				jsonResp["data"]["Long"] = telemetryObj.getLong();
+				jsonResp["data"]["lat"] = telemetryObj.getLat();
+				jsonResp["data"]["temp"] = telemetryObj.getTime();
+			}
 			
-			res.code = 200;
-			jsonResp["Status"] = "OK";
-			jsonResp["data"]["Long"] = telemetryObj.getLong();
-			jsonResp["data"]["lat"] = telemetryObj.getLat();
-			jsonResp["data"]["temp"] = telemetryObj.getTime();
 
 
 			res.set_header("Content-Type", "application/json");
